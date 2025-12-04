55ecca798860e11086b35f2b210dcd1b204bd9b0
YovanaAmelia <auccatomacruzyovana@gmail.com>
2025-12-04 09:42:04 -0500

    4/12/2025

--- xmlv1.php
+++ xmlv1.php
@@ -1,6 +1,7 @@
 <?php
 $ies = [];
-//
+
+//------------------UNIDADES DIDÁCTICAS-----------------//
 $udp1=[
 'FUNDAMENTOS DE PROGRAMACIÓN',
 'REDES E INTERNET',
@@ -9,7 +10,7 @@ $udp1=[
 'ARQUITECTURA DE COMPUTADORAS',
 'COMUNICACIÓN ORAL',
 'APLICACIONES EN INTERNET'];
-//
+
 $udp2=[ 
 'OFIMÁTICA',
 'INTERPRETACIÓN Y PRODUCCIÓN TEXTOS',
@@ -18,14 +19,14 @@ $udp2=[
 'ARQUITECTURA DE SERVIDORES WEB',
 'APLICACIONES SISTEMATIZADAS',
 'TALLER DE BASE DE DATOS'];
-//
+
 $udp3=[
 'ADMINISTRACIÓN DE BASE DE DATOS',
 'PROGRAMACIÓN DE APLICACIONES WEB',
 'DISEÑO DE INTERFACES WEB',
 'PRUEBAS DE SOFTWARE',
 'INGLÉS PARA LA COMUNICACIÓN ORAL'];
-//
+
 $udp4=[
 'DESARROLLO DE ENTORNOS WEB',
 'PROGRAMACIÓN DE SOLUCIONES WEB',
@@ -33,7 +34,7 @@ $udp4=[
 'SEGURIDAD EN APLICACIONES WEB',
 'COMPRENSIÓN Y REDACCIÓN EN INGLÉS',
 'COMPORTAMIENTO ÉTICO'];
-//
+
 $udp5=[
 'PROGRAMACIÓN DE APLICACIONES MÓVILES',
 'MARKETING DIGITAL',
@@ -42,7 +43,7 @@ $udp5=[
 'DIAGRAMACIÓN DIGITAL',
 'SOLUCIÓN DE PROBLEMAS',
 'OPORTUNIDADES DE NEGOCIOS'];
-//
+
 $udp6=[
 'PLATAFORMA DE SERVICIOS WEB',
 'ILUSTRACIÓN Y GRÁFICA DIGITAL',
@@ -51,78 +52,83 @@ $udp6=[
 'PLAN DE NEGOCIOS'];
 
 //------------------PERIODOS-----------------//
-$p1=[];
-$p1['nombre']="I";
-$p1['unidades_didacticas']= $udp1;
-
-$p2=array();
-$p2['nombre']="II";
-$p2['unidades_didacticas']= $udp2;
-
-$p3=[];
-$p3['nombre']="III";
-$p3['unidades_didacticas']= $udp3;
-
-$p4=[];
-$p4['nombre']="IV";
-$P4['unidades_didacticas']= $udp4;
-
-$p5=array();
-$p5['nombre']="V";
-$p5['unidades_didacticas']= $udp5;
-
-$p6=[];
-$p6['nombre']="VI";
-$p6['unidades_didacticas']= $udp6;
+$p1=['nombre'=>"I", 'unidades_didacticas'=> $udp1];
+$p2=['nombre'=>"II", 'unidades_didacticas'=> $udp2];
+$p3=['nombre'=>"III", 'unidades_didacticas'=> $udp3];
+$p4=['nombre'=>"IV", 'unidades_didacticas'=> $udp4]; // ← error corregido ($P4)
+$p5=['nombre'=>"V", 'unidades_didacticas'=> $udp5];
+$p6=['nombre'=>"VI", 'unidades_didacticas'=> $udp6];
 
 //---------MODULOS-------//
-$m1 = array();
-$m1['nombre']="ANÁLISIS Y DISEÑO DE SISTEMAS WEB";
-$m1['periodos']=[$p1 ,$p2];
+$m1 = ['nombre'=>"ANÁLISIS Y DISEÑO DE SISTEMAS WEB", 'periodos'=>[$p1 ,$p2]];
+$m2 = ['nombre'=>"DESARROLLO DE APLICACIONES WEB", 'periodos'=>[$p3 ,$p4]];
+$m3 = ['nombre'=>"DISEÑO DE SERVICIOS WEB", 'periodos'=>[$p5 ,$p6]];
 
-$m2 = array();
-$m2['nombre']="DESARROLLO DE APLICACIONES WEB";
-$m2['periodos']=[$p3 ,$p4];
-
-$m3 = array();
-$m3['nombre']="DISEÑO DE SERVICIOS WEB";
-$m3['periodos']=[$p5 ,$p6];
-//-----------PROGRAMAS DE ESTUDIOS------//
-$pe1= array();
-$pe1['nombre']="diseño y programacion web";
-$pe1['modulos']=[$m1,$m2,$m3];
-
-$pe2= array();
-$pe3= array();
-$pe4= array();
-$pe5= array();
+//-----------PROGRAMAS DE ESTUDIO------//
+$pe1= ['nombre'=>"diseño y programacion web", 'modulos'=>[$m1, $m2, $m3]];
+$pe2= ['nombre'=>"Enfermeria tecnica", 'modulos'=>[]];
+$pe3= ['nombre'=>"industrias de alimentos y bebidas", 'modulos'=>[]];
+$pe4= ['nombre'=>"mecanica automotriz", 'modulos'=>[]];
+$pe5= ['nombre'=>"produccion agropecuaria", 'modulos'=>[]];
 
 $ies['nombre']="IES Público HUANTA";
 $ies['programas_estudio']=[$pe1, $pe2, $pe3, $pe4, $pe5];
 
+//------------------------- CREAR XML --------------------------//
 $xml = new DOMDocument('1.0','UTF-8');
 $xml->formatOutput = true;
 
-$et1=$xml->createElement('ies');
+$et1 = $xml->createElement('ies');
 $xml->appendChild($et1);
 
+// Nombre IES
+$nombre_ies = $xml->createElement("nombre",$ies['nombre']);
+$programas_ies = $xml->createElement("programas_estudio");
+$et1->appendChild($nombre_ies);
+$et1->appendChild($programas_ies);
 
-    $nombre_ies =$xml->createElement("nombre",$ies['nombre']);
-    $programas_ies =$xml->createElement("programas_estudio");
-    foreach($ies["programas_estudio"] as $indice => $PEs) {
-       $num_pe = $xml->createElement("pe".$indice+1);
-        $nombre_pe = $xml->createElement("nombre",$PEs['nombre']);
-        $num_pe->appendChild($nombre_pe);
-        $programas_ies->appendChild($num_pe);
-        
+// PROGRAMAS DE ESTUDIO
+foreach($ies["programas_estudio"] as $indicePE => $PEs) {
 
+    $num_pe = $xml->createElement("pe".($indicePE+1));
+    $nombre_pe = $xml->createElement("nombre",$PEs['nombre']);
+    $num_pe->appendChild($nombre_pe);
+
+    // MODULOS
+    foreach ($PEs['modulos'] as $indice_modulo => $Modulo){
+        $num_mod = $xml->createElement("mod".($indice_modulo+1));
+        $nom_mod = $xml->createElement("nombre",$Modulo['nombre']);
+        $num_mod->appendChild($nom_mod);
+
+        // PERIODOS
+        foreach ($Modulo['periodos'] as $indice_periodo=>$Periodo){
+            $num_per = $xml->createElement("per".($indice_periodo+1));
+            $nom_per = $xml->createElement("nombre",$Periodo['nombre']);
+            $num_per->appendChild($nom_per);
+
+            // Unidades Didácticas
+            $uds = $xml->createElement("unidades_didacticas");
+
+            foreach ($Periodo['unidades_didacticas'] as $indice_ud => $Ud){ 
+                $num_ud = $xml->createElement("ud".($indice_ud+1));
+                $nom_ud = $xml->createElement("nombre",$Ud);
+                $num_ud->appendChild($nom_ud);
+                $uds->appendChild($num_ud);
+            }
+            $num_per->appendChild($nom_per);
+            $num_per->appendChild($uds);
+            $num_mod->appendChild($num_per);
+        }
+        $num_mod->appendChild($nom_mod);
+        $num_pe->appendChild($num_mod);
     }
-
-
+    $num_pe->appendChild($nombre_pe);
+    $programas_ies->appendChild($num_pe);
+}
 
 $archivo = "ies.xml";
 $xml->save($archivo);
 
-//print_r($pe1);
+
 
 ?>
